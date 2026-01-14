95484a201de052a460906bb2538182aeba3a1f3d
Arya Emami <aryaemami59@yahoo.com>
2026-01-13 21:37:25 -0600

    fix test issues

--- lib/rules.ts
+++ lib/rules.ts
@@ -10,13 +10,13 @@ import noUse from "./rules/no-use.ts"
 import requireDescription from "./rules/require-description.ts"
 
 export const rules = {
-    "disable-enable-pair": disableEnablePair,
-    "no-aggregating-enable": noAggregatingEnable,
-    "no-duplicate-disable": noDuplicateDisable,
-    "no-restricted-disable": noRestrictedDisable,
-    "no-unlimited-disable": noUnlimitedDisable,
-    "no-unused-disable": noUnusedDisable,
-    "no-unused-enable": noUnusedEnable,
-    "no-use": noUse,
-    "require-description": requireDescription,
+    "disable-enable-pair": disableEnablePair as Rule.RuleModule,
+    "no-aggregating-enable": noAggregatingEnable as Rule.RuleModule,
+    "no-duplicate-disable": noDuplicateDisable as Rule.RuleModule,
+    "no-restricted-disable": noRestrictedDisable as Rule.RuleModule,
+    "no-unlimited-disable": noUnlimitedDisable as Rule.RuleModule,
+    "no-unused-disable": noUnusedDisable as Rule.RuleModule,
+    "no-unused-enable": noUnusedEnable as Rule.RuleModule,
+    "no-use": noUse as Rule.RuleModule,
+    "require-description": requireDescription as Rule.RuleModule,
 } satisfies Record<string, Rule.RuleModule>
